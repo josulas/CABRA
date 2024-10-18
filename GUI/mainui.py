0d33109374deb0c23bd710466fc24a3a560eab12
Gonzalo Grau <ggrau@itba.edu.ar>
2024-10-18 20:38:57 -0300

    kicad

--- GUI/mainui.py
+++ GUI/mainui.py
@@ -1,25 +1,20 @@
 import sys
 from PySide6.QtWidgets import QApplication
 import pyqtgraph as pg
+import numpy as np
 
 uiclass, baseclass = pg.Qt.loadUiType("template.ui")
 
 class MainWindow(uiclass, baseclass):
-    def __init__(self):
+    def __init__(self, data):
         super().__init__()
         self.setupUi(self)
-        freqs = [10, 100, 1000, 10000]
-        self.widget.plot(freqs,
-                         [4, 5, 4, 3],
-                         pen='red',
-                         symbol='x')
-        self.widget.setLogMode(x=True, y=False)
-        self.widget.setXRange(1, 4)
+        self.widget.plot(data, pen='red')
         self.widget.showGrid(x=True, y=True, alpha=0.3)
 
 def main():
     app = QApplication(sys.argv)
-    window = MainWindow()
+    window = MainWindow(data=np.linspace(0, 100, 10))
     window.show()
     app.exec()
 
