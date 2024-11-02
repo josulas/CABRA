f366959798544d9fc594907e12efb3f0237d05b6
gonza <ggrau@itba.edu.ar>
2024-11-02 19:46:25 -0300

    baby gui steps

--- GUI/mainui.py
+++ GUI/mainui.py
@@ -1,22 +1,39 @@
 import sys
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QMainWindow
 import pyqtgraph as pg
 import numpy as np
 from template import Ui_MainWindow
+from playaudio import Clicker, EarSelect, CLICK_DURATION, CYCLE_DURATION, NCLICKS
+from simserial import Actions
 
 
-class MainWindow(Ui_MainWindow):
-    def __init__(self, data):
+class MainWindow(Ui_MainWindow, QMainWindow):
+    def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.plotWidget.plot(data, pen='red')
-        self.plotWidget.showGrid(x=True, y=True, alpha=0.3)
+        self.pushRUN.clicked.connect(self._print_msg)
 
-def run_ui(data):
+    def get_ear_selection(self):
+        return EarSelect.LEFT if self.radioLeftEAR.isChecked() \
+            else EarSelect.RIGHT if self.radioRightEAR.isChecked() \
+            else EarSelect.BOTH
+
+    def get_msg(self):
+        ear = self.get_ear_selection()
+        freq_idx = self.comboBoxFreq.currentIndex()
+        dbamp = int(self.comboBoxAmp.currentText())
+        return f"{Actions.RECORD} {NCLICKS} {freq_idx} {ear} {dbamp} {CLICK_DURATION} {CYCLE_DURATION}"
+
+    def _print_msg(self):
+        print(self.get_msg())
+
+
+def run_ui():
     app = QApplication(sys.argv)
-    window = MainWindow(data)
+    window = MainWindow()
     window.show()
     app.exec()
 
+
 if __name__ == '__main__':
-    run_ui(data=np.linspace(0, 100, 10))
+    run_ui()
