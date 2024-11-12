b7aad2492b23c27a379b2cc1009d818fa8aff163
Gonzalo Grau <ggrau@itba.edu.ar>
2024-11-12 00:54:33 -0300

    cabrasweep now working

--- GUI/desktop_ui.py
+++ GUI/desktop_ui.py
@@ -13,21 +13,25 @@ NCLICKS = 5
 SAMPLINGRATE = 10_000  # Hz (DO NOT CHANGE)
 OUTPUT_DIR = 'saved_audiometries'
 
-
-class MainWindow(Ui_MainWindow, QMainWindow):
+class CABRA_Window(Ui_MainWindow, QMainWindow):
     # Define states
     STATE_IDLE = 0
     STATE_RUNNING = 1
     STATE_WAITING_FOR_USER = 2
     STATE_COMPLETED = 3
 
+    # Class variables
     recording_completed = pyqtSignal()
-    valid_amplitudes = list(range(-10, 50, 10))
+    valid_amplitudes = list(range(-10, 45, 5))
 
     def __init__(self, process_path):
+        # Initial setup
         super().__init__()
         self.setupUi(self)
-        self.state = MainWindow.STATE_IDLE
+        self.state = CABRA_Window.STATE_IDLE
+        self.current_freq_idx = 0
+        self.current_ear = EarSelect.LEFT
+        self.in_CABRASweep = False
 
         # Plot setup
         self.evoked_X_axis = np.linspace(0, CYCLE_DURATION, CYCLE_DURATION * SAMPLINGRATE // 1000)
@@ -38,7 +42,7 @@ class MainWindow(Ui_MainWindow, QMainWindow):
         self.audiogram_figure_ready = False
 
         # Amplitude setup, initialize to midpoint
-        self.amplitudes = MainWindow.valid_amplitudes
+        self.amplitudes = CABRA_Window.valid_amplitudes
         self.id_min = 0
         self.id_max = len(self.amplitudes) - 1
         self.id_mid = (self.id_min + self.id_max) // 2
@@ -64,9 +68,9 @@ class MainWindow(Ui_MainWindow, QMainWindow):
         self.recording_completed.connect(self.handle_recording_completed)
 
         # pushEVOKED and pushNOISE are disabled until recording is completed
-        self.pushEVOKED.clicked.connect(self.set_heard)
+        self.pushEVOKED.clicked.connect(self.go_quieter)
         self.pushEVOKED.setEnabled(False)
-        self.pushNOISE.clicked.connect(self.set_not_heard)
+        self.pushNOISE.clicked.connect(self.go_louder)
         self.pushNOISE.setEnabled(False)
         self.heard = False
 
@@ -94,36 +98,24 @@ class MainWindow(Ui_MainWindow, QMainWindow):
         """
         self.spinClickDuration.setMaximum(self.spinCycleDuration.value())
 
-    def reset_dbamp(self):
-        # Reset the amplitude range
-        self.id_min = 0
-        self.id_max = len(self.amplitudes) - 1
-        self.id_mid = (self.id_min + self.id_max) // 2
-        self.dbamp = self.amplitudes[self.id_mid]
 
-    def update_mid_dbapm(self):
-        self.prev_dbamp = self.dbamp
-        self.id_mid = (self.id_min + self.id_max) // 2
-        self.dbamp = self.amplitudes[self.id_mid]
-
-    def set_heard(self):
-        self.heard = True
-        self.id_max = self.id_mid
-        self.update_mid_dbapm()
-        self.perform_audiometry_test()
-
-    def set_not_heard(self):
-        self.heard = False
-        self.id_min = self.id_mid + 1
-        self.update_mid_dbapm()
-        self.perform_audiometry_test()  # ugliest recursion ever
+    ##################################################################################################
+    # The following methods are related to the communication with the process and the GUI operation #
+    ##################################################################################################
 
     def get_ear_selection(self):
+        """
+        Translate the radio buttons to the EarSelect enum
+        """
         return EarSelect.LEFT if self.radioLeftEAR.isChecked() \
             else EarSelect.RIGHT if self.radioRightEAR.isChecked() \
             else EarSelect.BOTH
 
     def get_msg(self):
+        """
+        Get the message to send to the process, of the form
+        RECORD nclicks freq_idx ear dbamp click_duration cycle_duration
+        """
         ear = self.get_ear_selection()
         freq_idx = self.comboBoxFreq.currentIndex()
         dbamp = self.dbamp
@@ -133,16 +125,23 @@ class MainWindow(Ui_MainWindow, QMainWindow):
         return f"{Actions.RECORD} {nclicks} {freq_idx} {ear} {dbamp} {click_duration} {cycle_duration} \n"
 
     def start_process(self):
+        """
+        Initialize the process to communicate with the ESP board (or simulation) script
+        """
         self.process.start("python", [self.process_path])
         started = self.process.waitForStarted(500)
         if not started:
             self.labelStatus.setText("Failed to start process.")
             sys.exit(1)
 
+        # Automatically read stdout and stderr when something new shows up
         self.process.readyReadStandardOutput.connect(self.handle_stdout)
         self.process.readyReadStandardError.connect(self.handle_stderr)
 
     def _print_msg(self):
+        """
+        Just for debugging purposes
+        """
         print(self.get_msg())
 
     @pyqtSlot()
@@ -194,6 +193,10 @@ class MainWindow(Ui_MainWindow, QMainWindow):
         self.pushNOISE.setEnabled(True)
         # Plot
         self.plot_evoked()
+
+    ##############################################################################################
+    # The following methods are related to the audiometry test, and the audiogram plotting/saving #
+    ##############################################################################################
 
     def audiogram_is_ready(self):
         """
@@ -249,7 +252,7 @@ class MainWindow(Ui_MainWindow, QMainWindow):
         self.plotWidget.clear()
         self.plotWidget.plot(self.audiogram_left, pen='green', symbol='o', name='Left Ear')
         self.plotWidget.plot(self.audiogram_right, pen='blue', symbol='x', name='Right Ear')
-        self.plotWidget.setXRange(0, len(self.audiogram_left) - 1, padding=0.)
+        self.plotWidget.setXRange(0, len(self.audiogram_left) - 1, padding=0.05)
         self.plotWidget.setTitle(f"Audiogram for {self.nameEdit.text()}")
 
         # Axis setup
@@ -263,6 +266,51 @@ class MainWindow(Ui_MainWindow, QMainWindow):
         # Update the audiogram's save status
         self.audiogram_figure_ready = True
 
+    ##############################################################################################
+    # The following methods are related to the actual audiometry test, and the trademark CABRASweep #
+    ##############################################################################################
+
+    def reset_dbamp(self):
+        """
+        Reset the amplitude range to the initial state
+        """
+        self.id_min = 0
+        self.id_max = len(self.amplitudes) - 1
+        self.id_mid = (self.id_min + self.id_max) // 2
+        self.dbamp = self.amplitudes[self.id_mid]
+
+    def update_mid_dbapm(self):
+        """
+        Update the amplitude to the midpoint of the current range. Keep track of the last amplitude.
+        """
+        self.prev_dbamp = self.dbamp
+        self.id_mid = (self.id_min + self.id_max) // 2
+        self.dbamp = self.amplitudes[self.id_mid]
+
+    def go_quieter(self):
+        """
+        This method is called when the user indicates that the sound was heard.
+        It will update the amplitude range, and perform the next audiometry test.
+        Notice that the method updated the midpoint in a binary search fashion.
+        This method is called by the pushEVOKED button
+        """
+        self.heard = True
+        self.id_max = self.id_mid
+        self.update_mid_dbapm()
+        self.state = CABRA_Window.STATE_RUNNING
+        self.perform_audiometry_test()
+
+    def go_louder(self):
+        """
+        Analogous to the go_quieter method, but for when the sound was not heard.
+        It will update the amplitude range, and perform the next audiometry test.
+        This method is called by the pushNOISE button
+        """
+        self.heard = False
+        self.id_min = self.id_mid + 1
+        self.update_mid_dbapm()
+        self.state = CABRA_Window.STATE_RUNNING
+        self.perform_audiometry_test()  # ugliest recursion ever. I hope my future employers don't find this code
     def perform_audiometry_test(self):
         """
         For a given frequency and ear, perform the audiometry test to detect the hearing threshold,
@@ -271,69 +319,138 @@ class MainWindow(Ui_MainWindow, QMainWindow):
         NOTE: This function is recursive, and will call itself until the audiometry test is completed.
         """
 
-        if self.id_min < self.id_max:
-            self.pushRUN.setEnabled(False)
-            self.start_recording()
+        if self.state == CABRA_Window.STATE_RUNNING:
+        # Normal operation, perform the audiometry test using binary search
+            if self.id_min < self.id_max:
+                # Record ABR for the current amplitude
+                self.pushRUN.setEnabled(False)
+                self.start_recording()
+                self.state = CABRA_Window.STATE_WAITING_FOR_USER
 
-        else:
-            # Disable the evoked/noise buttons
-            self.pushEVOKED.setEnabled(False)
-            self.pushNOISE.setEnabled(False)
-            self.pushRUN.setEnabled(True)
-
-            # Update the status label and the audiogram
-            frequency = self.comboBoxFreq.currentText()
-            ear = 'left' if self.radioLeftEAR.isChecked() else 'right' if self.radioRightEAR.isChecked() else 'both'
-            self.labelStatus.setText(f"Hearing threshold for {ear} ear at frequency = {frequency} [Hz]:"
-                                     f" {self.prev_dbamp} [dbHL]")
-
-            if ear == 'left':
-                self.audiogram_left[self.comboBoxFreq.currentIndex()] = self.prev_dbamp
-            elif ear == 'right':
-                self.audiogram_right[self.comboBoxFreq.currentIndex()] = self.prev_dbamp
-
-            self.reset_dbamp()
-
-            # Check if the audiogram is ready to be saved
-            print(self.audiogram_left)
-            print(self.audiogram_right)
-            if self.audiogram_is_ready():
-                self.audiogram_ready_popup()
-                self.plot_audiogram()
-                self.save_audiogram()
             else:
-                self.plotWidget.clear()
-                self.plotWidget.plot(self.evoked_X_axis, self.nulldata, pen='red')
+                # Binary seach converged
+                self.complete_current_test()
 
+        elif self.state == CABRA_Window.STATE_WAITING_FOR_USER:
+            # Wait for the user to indicate if the sound was heard (handled by pushEVOKED and pushNOISE)
+            # For further clarification, refer to the go_quieter and go_louder methods.
+            pass
+
+        elif self.state == CABRA_Window.STATE_COMPLETED:
+            # Reset the state
+            self.complete_current_test()
+
+    def complete_current_test(self):
+        """
+        This method is called once the binary search performed in 'perform_audiometry_test' has converged.
+        It will update the audiogram and reset the amplitude range.
+        If the CABRASweep is active, it will move on to the next test.
+        """
+
+        # Disable the evoked/noise buttons
+        self.pushEVOKED.setEnabled(False)
+        self.pushNOISE.setEnabled(False)
+        self.pushRUN.setEnabled(True)
+
+        # Update the status label and the audiogram
+        frequency = self.comboBoxFreq.currentText()
+        ear = 'left' if self.radioLeftEAR.isChecked() else 'right' if self.radioRightEAR.isChecked() else 'both'
+        self.labelStatus.setText(f"Hearing threshold for {ear} ear at frequency = {frequency} [Hz]:"
+                                 f" {self.prev_dbamp} [dbHL]")
+
+        # Update the audiogram threshold level for the relevant frequency
+        if ear == 'left':
+            self.audiogram_left[self.comboBoxFreq.currentIndex()] = self.prev_dbamp
+        elif ear == 'right':
+            self.audiogram_right[self.comboBoxFreq.currentIndex()] = self.prev_dbamp
+
+        self.reset_dbamp()
+
+        # Check if the audiogram is ready to be saved
+        if self.audiogram_is_ready():
+            self.audiogram_ready_popup()
+            self.plot_audiogram()
+            self.save_audiogram()
+        else:
+            self.plotWidget.clear()
+            self.plotWidget.plot(self.evoked_X_axis, self.nulldata, pen='red')
+
+        # Continue the CABRASweep if necessary
+        if self.in_CABRASweep:
+            self.move_to_next_test()
+
+    def move_to_next_test(self):
+        """
+        Move to the next test in the CABRASweep, by updating ear and frequency.
+        If done, finish the CABRASweep.
+        """
+
+        assert self.in_CABRASweep
+
+        # Swap ears, or move to the next frequency
+        if self.current_ear == EarSelect.LEFT:
+            self.current_ear = EarSelect.RIGHT
+        else:
+            self.current_ear = EarSelect.LEFT
+            self.current_freq_idx += 1
+
+        # If there are more tests to perform
+        if self.current_freq_idx < self.comboBoxFreq.count():
+            # The next few lines, though scary looking, only update the input widgets to the next test
+            self.comboBoxFreq.setCurrentIndex(self.current_freq_idx)
+            if self.current_ear == EarSelect.LEFT:
+                self.radioLeftEAR.setChecked(True)
+            elif self.current_ear == EarSelect.RIGHT:
+                self.radioRightEAR.setChecked(True)
+
+            # Perform the next test
+            self.state = CABRA_Window.STATE_RUNNING
+            self.perform_audiometry_test()
+
+        # Finish off the CABRASweep
+        else:
+            self.state = CABRA_Window.STATE_IDLE
+            self.labelStatus.setText("CABRASweep completed.")
+            for widget in [self.pushCABRASweep, self.pushRUN, self.spinClickDuration,
+                           self.spinCycleDuration, self.checkBone, self.nameEdit,
+                           self.dateEdit, self.lineID]:
+                widget.setEnabled(True)
+            self.in_CABRASweep = False
+
+    # da GOAT
     def CABRASweep(self):
         """
-        Perform a sweep of the audiometry test for all frequencies and ears
+        Sweep over all frequencies and ears to perform the audiometry test.
+        This function relies heavily on the recursive function 'perform_audiometry_test', as well
+        as the 'move_to_next_test' function.
         """
-        for widget in [self.pushCABRASweep,  self.pushRUN, self.spinClickDuration,
+
+        assert self.state == CABRA_Window.STATE_IDLE and self.in_CABRASweep == False
+        self.in_CABRASweep = True
+
+        # Disable all widgets that shouldn't be touched
+        for widget in [self.pushCABRASweep, self.pushRUN, self.spinClickDuration,
                        self.spinCycleDuration, self.checkBone, self.nameEdit,
                        self.dateEdit, self.lineID]:
             widget.setEnabled(False)
 
-        for freq_idx in range(self.comboBoxFreq.count()):
-            print(f"{freq_idx=}")
-            self.comboBoxFreq.setCurrentIndex(freq_idx)
-            for ear in [EarSelect.LEFT, EarSelect.RIGHT]:
-                print(f"{ear=}")
-                if ear == EarSelect.LEFT:
-                    self.radioLeftEAR.setChecked(True)
-                elif ear == EarSelect.RIGHT:
-                    self.radioRightEAR.setChecked(True)
-                self.perform_audiometry_test()
+        # Initialize the test
+        self.current_freq_idx = 0
+        self.current_ear = EarSelect.LEFT
+        self.state = CABRA_Window.STATE_RUNNING
+        self.perform_audiometry_test()
 
-    # Kill the process when the window is closed
     def closeEvent(self, event):
+        """
+        Kill the process when the window is closed
+        """
         self.process.kill()
         event.accept()
 
 
 def run_ui():
     app = QApplication(sys.argv)
-    window = MainWindow(process_path='simserial.py')
+    window = CABRA_Window(process_path='simserial.py')
     window.show()
     app.exec()
 
